f5c858ed41bba011f0785724a0f0529f604952a9
Cássio Oliveira <cassiosume@gmail.com>
2013-11-28 22:52:15 -0200

    Main class created.

---
+++ src/br/com/ambiciousteam/pfbistro/main/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.ambiciousteam.pfbistro.main;
+
+/**
+ * @author Eclipse
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
