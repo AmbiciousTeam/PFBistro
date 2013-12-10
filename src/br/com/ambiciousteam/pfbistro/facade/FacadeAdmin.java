1a1072f6b0eb3f6b99dd3581c58cd98f8ff4e032
Erivaldo-Wellington <wellingtonssdm@gmail.com>
2013-12-10 20:43:43 -0300

    new package for view and tests

---
+++ src/br/com/ambiciousteam/pfbistro/facade/FacadeAdmin.java
@@ -0,0 +1,11 @@
+package br.com.ambiciousteam.pfbistro.facade;
+
+import br.com.ambiciousteam.pfbistro.model.Administrator;
+
+public interface FacadeAdmin {
+	
+	public boolean login(Administrator admin);
+	
+	
+	
+}
---
+++ src/br/com/ambiciousteam/pfbistro/view/ViewAdmin.java
@@ -0,0 +1,60 @@
+package br.com.ambiciousteam.pfbistro.view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+public class ViewAdmin extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ViewAdmin frame = new ViewAdmin();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ViewAdmin() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnBar = new JMenu("Cadastrar");
+		menuBar.add(mnBar);
+		
+		JMenuItem mntmAd = new JMenuItem("Administrador");
+		mnBar.add(mntmAd);
+		
+		JMenuItem mntmProduto = new JMenuItem("Produto");
+		mnBar.add(mntmProduto);
+		
+		JMenuItem mntmCardapio = new JMenuItem("Cardapio");
+		mnBar.add(mntmCardapio);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
