69d371d6e6169ce47e204f072918d824333fd1b4
Maelby <maelbymunix@gmail.com>
2013-12-18 21:13:06 -0300

    Create class select and clientView

--- src/br/com/ambiciousteam/pfbistro/view/ViewClient.java
+++ src/br/com/ambiciousteam/pfbistro/view/ViewClient.java
@@ -1,25 +1,61 @@
 package br.com.ambiciousteam.pfbistro.view;
 
-import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+
+import org.hibernate.metamodel.source.binder.JpaCallbackClass;
+
+import br.com.ambiciousteam.pfbistro.dao.MenuTableModel;
+import br.com.ambiciousteam.pfbistro.dao.Queries;
+import br.com.ambiciousteam.pfbistro.enummeration.EnumCategories;
+import br.com.ambiciousteam.pfbistro.facade.FacadeAdmin;
+import br.com.ambiciousteam.pfbistro.facade.FacadeAdminImpl;
+import br.com.ambiciousteam.pfbistro.model.Product;
+
+import java.awt.Component;
 
 @SuppressWarnings("serial")
 public class ViewClient extends JFrame {
 
 	private JPanel contentPane;
+	private FacadeAdmin facade;
+	private JComboBox<String> comboMenuSelectCat;
+	private JComboBox<Product> comboMenuSelectItem;
+	private String selectedCategory;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					ViewClient frame = new ViewClient();
+					ViewAdmin frame = new ViewAdmin();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -32,12 +68,119 @@ public class ViewClient extends JFrame {
 	 * Create the frame.
 	 */
 	public ViewClient() {
+		facade = new FacadeAdminImpl();
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 640, 400);
+		getContentPane().setLayout(null);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane.setLayout(null);
 		setContentPane(contentPane);
+		setLocationRelativeTo(null);
+		setResizable(false);
+
+		JPanel panelTabbedFullReg = new JPanel();
+		panelTabbedFullReg.setBounds(0, 0, 634, 372);
+		contentPane.add(panelTabbedFullReg);
+		panelTabbedFullReg.setLayout(null);
+
+		JTabbedPane tabbedAdminFullReg = new JTabbedPane(JTabbedPane.TOP);
+		tabbedAdminFullReg.setBounds(0, 0, 634, 373);
+		panelTabbedFullReg.add(tabbedAdminFullReg);
+
+		JLayeredPane paneMenuReg = new JLayeredPane();
+		tabbedAdminFullReg.addTab("Cardápio", null, paneMenuReg, null);
+
+		JPanel panelMenuReg = new JPanel();
+		panelMenuReg.setLayout(null);
+		panelMenuReg.setBounds(0, 0, 629, 345);
+		paneMenuReg.add(panelMenuReg);
+
+		JLabel lblCadastroDeCardpio = new JLabel("Card\u00E1pio");
+		lblCadastroDeCardpio.setHorizontalAlignment(SwingConstants.CENTER);
+		lblCadastroDeCardpio.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblCadastroDeCardpio.setBounds(106, 11, 513, 45);
+		panelMenuReg.add(lblCadastroDeCardpio);
+
+		JPanel panelSelectItens = new JPanel();
+		panelSelectItens.setBounds(10, 67, 609, 54);
+		panelMenuReg.add(panelSelectItens);
+		panelSelectItens.setLayout(null);
+
+		JLabel lblMenuSelectCat = new JLabel("Selecione uma categoria");
+		lblMenuSelectCat.setHorizontalAlignment(SwingConstants.CENTER);
+		lblMenuSelectCat.setFont(new Font("Tahoma", Font.BOLD, 12));
+
+		lblMenuSelectCat.setBounds(0, 0, 180, 14);
+		panelSelectItens.add(lblMenuSelectCat);
+
+		comboMenuSelectItem = new JComboBox<>();		
+		comboMenuSelectItem.setBounds(190, 25, 330, 20);
+
+		comboMenuSelectCat = new JComboBox<String>();
+		comboMenuSelectCat.setBounds(0, 25, 180, 20);
+		comboMenuSelectCat.addItem(EnumCategories.DRINKS.getCategory());
+		comboMenuSelectCat.addItem(EnumCategories.SNACK.getCategory());
+		comboMenuSelectCat.addItem(EnumCategories.PIZZA.getCategory());
+		panelSelectItens.add(comboMenuSelectCat);
+		setSelectedCategory(comboMenuSelectCat.getSelectedItem().toString());
+
+		//		System.out.println(comboMenuSelectCat.);
+		//		comboMenuSelectCat.addMouseListener(new java.awt.event.MouseAdapter() {
+		//			public void mouseClicked(java.awt.event.MouseEvent evt) {
+		//				while (true) {
+		//comboMenuSelectCat.getSelectedItem().
+		//System.out.println(getSelectedCategory());
+		//				}
+		//			}
+		//		});
+
+		//FALTA CHAMAR A CONSULTA
+		//		comboMenuSelectCat.addActionListener(new ActionListener() {
+
+		//			@Override
+		//			public void actionPerformed(ActionEvent arg0) {
+		//				setSelectedCategory(comboMenuSelectCat.getSelectedItem().toString());
+		//				facade.newQuery
+		//				List<Product> queryProdByCat = q.queryProdByCat(getSelectedCategory());
+		//				for (Product product : queryProdByCat) {
+		//					comboMenuSelectItem.addItem(product);
+		//				}
+		//				
+		//			}
+		//		});
+
+		JLabel lblMenuSelectItem = new JLabel("Selecione um item");
+		lblMenuSelectItem.setHorizontalAlignment(SwingConstants.CENTER);
+		lblMenuSelectItem.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblMenuSelectItem.setBounds(190, 0, 328, 14);
+		panelSelectItens.add(lblMenuSelectItem);
+
+
+		panelSelectItens.add(comboMenuSelectItem);
+
+		JButton btnMenuInsert = new JButton("Inserir");
+		btnMenuInsert.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnMenuInsert.setBounds(528, 25, 81, 22);
+		panelSelectItens.add(btnMenuInsert);
+
+		JPanel panelMountedMenu = new JPanel();
+		panelMountedMenu.setBounds(10, 132, 609, 202);
+		panelMenuReg.add(panelMountedMenu);
+		panelMountedMenu.setLayout(null);
+
+		JScrollPane scrollMenuItens = new JScrollPane();
+		scrollMenuItens.setBounds(0, 0, 609, 202);
+		panelMountedMenu.add(scrollMenuItens);
+
 	}
 
+	public String getSelectedCategory() {
+		return selectedCategory;
+	}
+
+	public void setSelectedCategory(String selectedCategory) {
+		this.selectedCategory = selectedCategory;
+	}
 }
